769e1c8b30c307a835fc8b8a6d48a60e56572d57
mehadihasan00 <meahdihasan.rajib10@gmail.com>
2021-03-15 20:24:13 +0600

    Make a simple Clock Projects

---
+++ Think.py
@@ -0,0 +1,21 @@
+from tkinter import*
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Clock Try")
+
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+
+label = Label(root, font=("Fira code", 80),
+              background='black', foreground='cyan')
+label.pack(anchor='center')
+
+time()
+mainloop()
