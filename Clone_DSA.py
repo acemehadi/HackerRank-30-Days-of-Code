2c37854bc0ae71e11d3bc969c5a3736d07fae867
mehadihasan00 <meahdihasan.rajib10@gmail.com>
2021-03-16 14:41:57 +0600

    Data Structure creat stack

--- Clone_DSA.py
+++ Clone_DSA.py
@@ -1,25 +1,26 @@
-# Stack implementation in python
+# stack implementation in pyhton
 
-# Creating a stack
+# creating a stack
 def create_stack():
     stack = []
     return stack
 
 
-# Creating an empty stack
+# creating an empty stack
 def check_empty(stack):
     return len(stack) == 0
 
+# Adduing items into the stack
 
-# Adding items into the stack
+
 def push(stack, item):
     stack.append(item)
-    print("pushed item: " + item)
+    print("Pushed item"+item)
 
 
 # Removing an element from the stack
 def pop(stack):
-    if (check_empty(stack)):
+    if(check_empty(stack)):
         return "stack is empty"
 
     return stack.pop()
@@ -30,5 +31,5 @@ push(stack, str(1))
 push(stack, str(2))
 push(stack, str(3))
 push(stack, str(4))
-print("popped item: " + pop(stack))
-print("stack after popping an element: " + str(stack))
+print("Popped item"+pop(stack))
+print("stack after popping and element :"+str(stack))
--- Helllow.py
+++
@@ -1,7 +0,0 @@
-def my_calculator(x):
-  return 5*x
-
-print(my_calculator(1))
-print(my_calculator(2))
-print(my_calculator(3))
-
