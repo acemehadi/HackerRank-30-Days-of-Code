5b2c07d9cd18668fa695b0ca2ca3170b0aa817b2
mehadihasan00 <meahdihasan.rajib10@gmail.com>
2021-04-21 11:35:12 +0600

    Day 16: Exceptions - String to Integer

--- HackerRank_Solution.py
+++ HackerRank_Solution.py
@@ -1,31 +1,9 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+import sys
 
+s = input(":").strip()
 
-class Solution:
-    def display(self, head):
-        current = head
-        while current:
-            print(current.data, end=' ')
-            current = current.next
-
-    def insert(self, head, data):
-        if head is None:
-            head = Node(data)
-        else:
-            curr = head
-            while curr.next:
-                curr = curr.next
-            curr.next = Node(data)
-        return head
-
-
-mylist = Solution()
-T = int(input())
-head = None
-for i in range(T):
-    data = int(input())
-    head = mylist.insert(head, data)
-mylist.display(head)
+try:
+    s = int(s)
+    print(s)
+except:
+    print("Bad String")
